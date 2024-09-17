ce673e5b49b69dc24d4bb291048f8e843f399eb0
William Stroff <william.stroff@elev.ga.ntig.se>
2024-09-17 14:55:48 +0200

    First commit

---
+++ monthly_payment.py
@@ -0,0 +1,16 @@
+import os
+# Clear
+os.system("cls")
+
+# Calculate monthly payment
+def calculate_monthly_payment(principal, annual_rate, years):
+    rate = annual_rate / 12 / 100
+    months = years * 12
+    payment = (principal * rate) / (1- (1 + rate) ** -months)
+    return(payment)
+
+principal = float(input("Lånebelopp: "))
+annual_rate = float(input("Årsränta (%): "))
+years = int(input("Löptid (år): "))
+payment = calculate_monthly_payment(principal, annual_rate, years)
+print(f"Din månatliga betalning är: {payment:.2f} kr/Månad")
